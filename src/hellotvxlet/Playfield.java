c4a7874611233a66b86f5b8ffc0489f73bbd52ab
bjarnex3 <bjarne.willems@gmail.com>
2017-05-18 15:47:28 +0200

    bounds

--- src/hellotvxlet/Playfield.java
+++ src/hellotvxlet/Playfield.java
@@ -15,6 +15,7 @@ import org.dvb.event.UserEventListener;
 import org.dvb.event.UserEventRepository;
 import org.havi.ui.HComponent;
 
+
 /**
  *
  * @author student
@@ -25,39 +26,74 @@ public class Playfield extends HComponent implements UserEventListener {
     int hx=100;
     int hy=100;
     int r=1;    //richting
+    int length= 6;
+    Graphics apple;
+    SlangSegment s;
+    boolean hasCollided = false;
     
     public Playfield()
     {
         this.setBounds(0,0,720,576); // full screen
         
+        
     }
     
     public void paint(Graphics g)
     {
+        
+        
         for (int i=0;i<slang.size();i++)
         {
             g.setColor(Color.GREEN);
-            SlangSegment s=new SlangSegment();
+            s=new SlangSegment();
             s=(SlangSegment)slang.get(i);
             
             g.fillOval(s.x,s.y , 10, 10);
-            
+                        
         }
+        
+        
     }
 
     
     public void run()
     {
-        SlangSegment s=new SlangSegment();
+        
+        
+        
+        s=new SlangSegment();
         if (r==1) hx = hx+10;   //rechts
         else if (r==2) hy = hy+10;  //onder
         else if (r==3) hx = hx-10;  //links
         else if (r==4) hy = hy-10;  //boven
         s.x=hx;
         s.y=hy;
-  
-        slang.add(s);
-        this.repaint();
+        
+        
+        
+        System.out.println(hasCollided);
+        checkCollision();
+        if(hasCollided == false){
+        
+            
+            
+            slang.add(s);
+            setLengthSnake(length);
+            this.repaint();
+        }
+        
+        
+        System.out.println(s.x);
+        System.out.println(s.y);
+            
+        
+        
+        
+        
+        
+        //setLengthSnake(length);
+        
+        //this.repaint();
     }
     public void userEventReceived(UserEvent e) {
         if(e.getCode() == HRcEvent.VK_LEFT && r!=1){
@@ -71,6 +107,34 @@ public class Playfield extends HComponent implements UserEventListener {
         }
         else if(e.getCode() == HRcEvent.VK_DOWN && r!=4){
             r = 2;
+        }
+        
+    }
+    
+    public void setLengthSnake(int maxLength){
+        if(slang.size() == maxLength){
+            slang.remove(0);
+        }
+    }
+    public void checkCollision(){
+        
+        for (int i=0;i<slang.size();i++)
+        {   
+            
+            int xCoordSlangDeel = ((SlangSegment)(slang.get(i))).x;
+            int xCoordHead = s.x;
+            int yCoordSlangDeel = ((SlangSegment)(slang.get(i))).y;
+            int yCoordHead = s.y;
+            
+            
+            if(xCoordSlangDeel == xCoordHead && yCoordSlangDeel == yCoordHead ){
+                
+                hasCollided = true;
+            }
+            else if(xCoordHead > 720 || xCoordHead < 0 || yCoordHead < 0 || yCoordHead < -580){
+                hasCollided = true;
+            }
+            
         }
         
     }
